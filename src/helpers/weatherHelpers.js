872775e249cee5ed9effa12e7fcc630d9045a96e
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-10-14 01:16:14 -0500

    Added Luxon library to handle date/times

--- src/helpers/weatherHelpers.js
+++ src/helpers/weatherHelpers.js
@@ -1,7 +1,22 @@
+import { DateTime, Zone } from "luxon";
+
 export const extractWeatherData = (data) => ({
   weather: data.weather[0],
   main: data.main,
   wind: data.wind,
   dt: data.dt,
+  timezone: data.timezone,
   visibility: data.visibility,
+  sunrise: data.sys.sunrise,
+  sunset: data.sys.sunset,
 });
+
+export const buildLocalDateTime = (offset) => {
+  const dt = DateTime.utc().toUTC(offset);
+  const date = dt.toFormat("DDDD");
+  const time = dt.toFormat("t");
+  console.log("DateTime", { DateTime });
+  return { date, time };
+};
+
+export const getLocalTime = (ms, offset) => {};
